b0180ab499b003e91dee1cd84fb7d06110350ae7
royalgraphx <39929362+royalgraphx@users.noreply.github.com>
2024-12-01 02:21:30 -0600

    Upgrade VMH Header file

--- VMHide/kern_start.hpp
+++ VMHide/kern_start.hpp
@@ -16,7 +16,10 @@
 #include <libkern/libkern.h>
 #include <sys/sysctl.h>
 
-// Macros for sysctl types
+/**
+* Macros for sysctl types, taken from RestrictEvents
+* https://github.com/acidanthera/RestrictEvents/blob/master/RestrictEvents/SoftwareUpdate.hpp
+*/
 #define CTLTYPE             0xf     // Mask for the type
 #define CTLTYPE_NODE        1       // name is a node
 #define CTLTYPE_INT         2       // name describes an integer
@@ -27,17 +30,63 @@
 
 #define SYSCTL_OUT(r, p, l) (r->oldfunc)(r, p, l)
 
-// Function prototypes
-void vmhInit();
-mach_vm_address_t parseSysctlChildren();
-bool reRouteHvVmm(mach_vm_address_t sysctlChildrenAddress);
-int vmh_sysctl_vmm_present(struct sysctl_oid *oidp, void *arg1, int arg2, struct sysctl_req *req);
+// Reworked VMH Class from the earlier prototypes
+class VMH {
+public:
+    /**
+     * Standard Init and deInit functions
+     */
+    void init();
+    void deinit();
+    
+    /**
+     *  Function to parse the sysctl children memory address
+     */
+    mach_vm_address_t parseSysctlChildren();
+    
+    /**
+     *  Function to reroute kern hv vmm present function to our own custom one in VMH
+     */
+    bool reRouteHvVmm(mach_vm_address_t sysctlChildrenAddress);
+    
+    /**
+     *  VMHide's custom sysctl VMM present function (with PID support)
+     */
+    int vmh_sysctl_vmm_present(struct sysctl_oid *oidp, void *arg1, int arg2, struct sysctl_req *req);
 
-// Enum to represent VMHide states
-enum VmhState {
-    VMH_DISABLED,
-    VMH_ENABLED,
-    VMH_PASSTHROUGH
-};
+    /**
+     * Enum to represent VMHide states
+     */
+    enum VmhState {
+        VMH_DISABLED,
+        VMH_ENABLED,
+        VMH_PASSTHROUGH
+    };
+
+private:
+    /**
+     *  Private self instance for callbacks
+     */
+    static VMH *callbackVMH;
+    
+    /**
+     *  Enable KernelPatcher for resolving symbols
+     *
+     *  @param patcher KernelPatcher instance
+     */
+    void processKernel(KernelPatcher &patcher);
+    
+    /**
+     *  Static variable to store the original kern hv vmm present handler
+     */
+    static sysctl_handler_t originalHvVmmHandler = nullptr;
+    
+    /**
+     *  Struct to hold both process name and potential PID
+     */
+    struct FilteredProcess {
+        const char *name;
+        pid_t pid;
+    };
 
 #endif /* kern_start_hpp */
