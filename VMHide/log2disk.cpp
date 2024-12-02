0e164c582f84c344f42e69a5c9ff6e4b3b7a4175
royalgraphx <39929362+royalgraphx@users.noreply.github.com>
2024-12-01 21:48:49 -0600

    Init blank L2D Contents in Xcode Proj

---
+++ VMHide/log2disk.cpp
@@ -0,0 +1,7 @@
+//
+//  log2disk.cpp
+//  VMHide
+//
+//  Created by RoyalGraphX on 12/1/24.
+//
+
---
+++ VMHide/log2disk.hpp
@@ -0,0 +1,7 @@
+//
+//  log2disk.hpp
+//  VMHide
+//
+//  Created by RoyalGraphX on 12/1/24.
+//
+
